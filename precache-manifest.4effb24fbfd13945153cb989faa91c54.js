3e830b1d9cc09ef631ebce80c9c4fbe4b12eeb5a
ali beik dolati <ali.beik2lati@gmail.com>
2020-09-06 12:47:48 +0430

    Updates

---
+++ precache-manifest.4effb24fbfd13945153cb989faa91c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7466953eb81c190b211f4b8b3f2714",
+    "url": "/simple_timer/index.html"
+  },
+  {
+    "revision": "f78cff331b3e76b0b652",
+    "url": "/simple_timer/static/css/main.152382ac.chunk.css"
+  },
+  {
+    "revision": "160d2832a5a2b2cc87bb",
+    "url": "/simple_timer/static/js/2.ac144fac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple_timer/static/js/2.ac144fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78cff331b3e76b0b652",
+    "url": "/simple_timer/static/js/main.19620972.chunk.js"
+  },
+  {
+    "revision": "cb2aff2aef266a734827",
+    "url": "/simple_timer/static/js/runtime-main.9a2fe66a.js"
+  }
+]);
